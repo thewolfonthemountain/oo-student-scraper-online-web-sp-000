6bd1559bb6fd737296314d7300702b35e4db2b51
Sean Langton <slangton1@gmail.com>
2018-11-20 21:09:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_array.each {|key, value| self.send(("#{key}="), value)
+    student_hash.each {|key, value| self.send(("#{key}="), value)
     @@all.push(self)
     }
   end
